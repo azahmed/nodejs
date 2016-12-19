917327ec919ef665678e9cf063bf57fb93cf3c28
Asrar Ahmed <asrar@asrar.com>
2016-12-19 12:01:12 +1100

    Done async file read i.e. non blocking code

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 var fs = require("fs");
-var data = fs.readFileSync('asrar.txt');
-console.log(data.toString());
+//var data = fs.readFileSync('asrar.txt');
+
+fs.readFile('asrar.txt', function(err, data) {
+	if(err) {
+		return console.error(err);
+	}
+	console.log(data.toString());
+});
+
+//console.log(data.toString());
 console.log('Done main.js!');
