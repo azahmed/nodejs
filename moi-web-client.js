77ac42f52d373e72f9ccb840a23f2bf0d6a88910
Asrar Ahmed <asrar@asrar.com>
2016-12-19 14:57:18 +1100

    Client Web not connecting with ECONNREFUSED

---
+++ moi-web-client.js
@@ -0,0 +1,28 @@
+// client.js
+var http = require('http');
+
+var options = {
+	host: 'localhost',
+	port: '8081',
+	path: 'index.htm'
+};
+
+// callback
+var callback = function(response) {
+	// cont update stream
+	var body = '';
+	
+	response.on('data', function(data) {
+		body += data;
+	});
+	
+	response.on('end', function() {
+		console.log(body);
+	});
+	
+}
+
+// call server
+var req = http.request(options, callback);
+req.end();
+
