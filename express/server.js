9eaf10d9b33402a7be69d28ce7e9d516291e7ce4
Asrar Ahmed <asrar@asrar.com>
2016-12-20 11:03:30 +1100

    Some routing

--- express/server.js
+++ express/server.js
@@ -3,8 +3,25 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('Ola');
+	res.send('Ola GET');
 })
+
+app.post('/', function(req, res) {
+	res.send('Ola POST');
+})
+
+app.delete('/deleteUser', function(req, res) {
+	res.send('Ola DELETE');
+})
+
+app.get('/listUser', function(req, res) {
+	res.send('Ola Listing');
+})
+
+app.get('/ab*yz', function(req, res) {
+	res.send('Ola ab*yz');
+})
+
 
 var server = app.listen(8081, function() {
 	var host = server.address().address
